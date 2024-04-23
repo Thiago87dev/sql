fa2e729856d1a3c4f4e797e4840c381a29f6cb52
thiago alves <thiago87gsa@gmail.com>
2024-04-22 23:10:24 -0300

    inner join

---
+++ 02joins/01innerJoin.sql
@@ -0,0 +1,8 @@
+-- seleciona registros que possuem valores correspondentes em ambas as tabelas
+
+SELECT a.nome_aluno, c.nome_curso 
+from aluno_curso as ac
+inner join alunos as a on a.id_aluno = ac.id_aluno
+inner join cursos as c on c.id_curso = ac.id_curso
+
+-- esta é uma relação de N para N por isso tem uma tabela intermediaria no caminho.
